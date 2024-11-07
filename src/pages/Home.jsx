44e3138045396419074d463451a2fcbe68f78232
Nurudeen <abdulmajeednurudeen48@gmail.com>
2024-11-07 23:00:37 +0000

    clearing everything to rebuild with new design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,7 @@
-import NavBar from '../components/NavBar';
-import Hero from '../components/Hero';
-import Projects from './Projects';
-import Skills from '../components/Skills';
-import Footer from '../components/Footer';
-
 const Home = () => {
   return (
     <div className="bg-gray-200">
-      <NavBar />
-      <Hero />
-      <Projects />
-      <Skills />
-      <Footer />
+      <h1>Portfolio</h1>
     </div>
   );
 };
