04f392b824930fedb318d04c8dc7c43e623d69ed
Nurudeen <abdulmajeednurudeen48@gmail.com>
2024-11-15 20:17:45 +0000

    fixed a bug with the side menu animation

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -45,12 +45,10 @@ const NavBar = () => {
 
   const handleScroll = () => {
     if (navRef.current) {
-      const { top, bottom } = navRef.current.getBoundingClientRect();
+      const { bottom } = navRef.current.getBoundingClientRect();
       const visible = bottom > 0;
 
-      if (visible !== isNavVisible) {
-        setIsNavVisible(visible);
-      }
+      setIsNavVisible(visible);
     }
   };
 
@@ -61,60 +59,60 @@ const NavBar = () => {
 
   return (
     <nav ref={navRef} className="bg-white">
-      <AnimatePresence mode="wait">
-        <div className="max-w-6xl mx-auto px-8 pt-8 pb-4" key={isNavVisible}>
-          <div className="flex justify-between items-center">
-            {/* Logo */}
+      <div className="max-w-6xl mx-auto px-8 pt-8 pb-4">
+        <div className="flex justify-between items-center">
+          {/* Logo */}
 
-            <div className="flex justify-between w-full">
-              <h1 className="text-2xl font-urbanist font-semibold text-black ">
-                Portfolio
-              </h1>
+          <div className="flex justify-between w-full">
+            <h1 className="text-2xl font-urbanist font-semibold text-black ">
+              Portfolio
+            </h1>
 
-              <p
-                onClick={toggleMenu}
-                className="md:hidden text-gray-800 text-xl font-urbanist font-semibold"
-              >
-                Menu
-              </p>
-            </div>
+            <p
+              onClick={toggleMenu}
+              className="md:hidden text-gray-800 text-xl font-urbanist font-semibold"
+            >
+              Menu
+            </p>
+          </div>
 
-            {/* Desktop Navigation - No Icons */}
-            <div className="hidden md:flex gap-6 font-urbanist">
-              {navItems.map((item) =>
-                item.external ? (
-                  <a
-                    key={item.href}
-                    href={item.href}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="transition-colors p-2 text-gray-700 font-semibold hover:text-gray-700 hover:cursor-pointer"
-                  >
-                    {item.label}
-                  </a>
-                ) : (
-                  <ScrollLink
-                    key={item.href}
-                    to={item.href}
-                    smooth={true}
-                    duration={500}
-                    offset={-50}
-                    onClick={() => handleActiveItem(item.label)}
-                    className={`transition-colors p-2 hover:cursor-pointer font-semibold text-lg ${
-                      activeItem === item.label
-                        ? 'text-gray-700'
-                        : 'text-black hover:text-gray-700'
-                    }`}
-                  >
-                    {item.label}
-                  </ScrollLink>
-                )
-              )}
-            </div>
+          {/* Desktop Navigation - No Icons */}
+          <div className="hidden md:flex gap-6 font-urbanist">
+            {navItems.map((item) =>
+              item.external ? (
+                <a
+                  key={item.href}
+                  href={item.href}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="transition-colors p-2 text-gray-700 font-semibold hover:text-gray-700 hover:cursor-pointer"
+                >
+                  {item.label}
+                </a>
+              ) : (
+                <ScrollLink
+                  key={item.href}
+                  to={item.href}
+                  smooth={true}
+                  duration={500}
+                  offset={-50}
+                  onClick={() => handleActiveItem(item.label)}
+                  className={`transition-colors p-2 hover:cursor-pointer font-semibold text-lg ${
+                    activeItem === item.label
+                      ? 'text-gray-700'
+                      : 'text-black hover:text-gray-700'
+                  }`}
+                >
+                  {item.label}
+                </ScrollLink>
+              )
+            )}
+          </div>
 
-            {/* Mobile Menu Button */}
-
+          {/* Mobile Menu Button */}
+          <AnimatePresence mode="wait">
             <motion.button
+              key={isNavVisible}
               initial={{ opacity: 0, scale: 0 }}
               animate={{ opacity: 1, scale: 1 }}
               exit={{ opacity: 0, scale: 0 }}
@@ -139,79 +137,79 @@ const NavBar = () => {
             >
               {isMenuOpen ? <X size={24} /> : <Menu size={24} />}
             </motion.button>
-          </div>
+          </AnimatePresence>
+        </div>
 
-          {/* Mobile Navigation - Right Side Drawer with Icons */}
-          <motion.div
-            initial={{ x: '100%' }}
-            animate={{ x: isMenuOpen ? 0 : '100%' }}
-            exit={{ x: '100%' }}
-            className={`fixed top-0 right-0 flex flex-col  px-12 py-24 lg:px-20 lg:py-12 h-dvh w-full md:w-1/2 bg-gray-950 shadow-lg  ${
-              isMenuOpen ? 'translate-x-0' : 'translate-x-full'
-            }  z-40`}
-          >
+        {/* Mobile Navigation - Right Side Drawer with Icons */}
+        <motion.div
+          initial={{ x: '100%' }}
+          animate={{ x: isMenuOpen ? 0 : '100%' }}
+          exit={{ x: '100%' }}
+          className={`fixed top-0 right-0 flex flex-col  px-12 py-24 lg:px-20 lg:py-12 h-dvh w-full md:w-1/2 bg-gray-950 shadow-lg  ${
+            isMenuOpen ? 'translate-x-0' : 'translate-x-full'
+          }  z-40`}
+        >
+          <div className="flex flex-col gap-10">
+            <p className="text-gray-400 text-2xl px-1.5 py-2.5 border-b border-gray-400">
+              Navigation
+            </p>
+
+            <div className="flex flex-col gap-5 font-urbanist">
+              {navItems.map((item) =>
+                item.external ? (
+                  <a
+                    key={item.href}
+                    href={item.href}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    onClick={closeMenu}
+                    className="text-4xl text-white px-1.5 py-2.5 hover:text-gray-400 transition-colors flex items-center gap-3"
+                  >
+                    {item.label}
+                  </a>
+                ) : (
+                  <ScrollLink
+                    key={item.href}
+                    to={item.href}
+                    smooth={true}
+                    duration={500}
+                    offset={-70}
+                    onClick={closeMenu}
+                    className="text-4xl text-white px-1.5 py-2.5  hover:text-gray-400 hover:cursor-pointer transition-colors  flex items-center gap-3"
+                  >
+                    {item.label}
+                  </ScrollLink>
+                )
+              )}
+            </div>
+
             <div className="flex flex-col gap-10">
-              <p className="text-gray-400 text-2xl px-1.5 py-2.5 border-b border-gray-400">
-                Navigation
-              </p>
+              <h2 className="text-2xl px-1.5 py-4 text-gray-400 border-b border-gray-400">
+                Socials
+              </h2>
+              <div className="flex flex-wrap gap-6 text-white">
+                <div>
+                  <a href="https://web.facebook.com/profile.php?id=100080955712476">
+                    Facebook
+                  </a>
+                </div>
 
-              <div className="flex flex-col gap-5 font-urbanist">
-                {navItems.map((item) =>
-                  item.external ? (
-                    <a
-                      key={item.href}
-                      href={item.href}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      onClick={closeMenu}
-                      className="text-4xl text-white px-1.5 py-2.5 hover:text-gray-400 transition-colors flex items-center gap-3"
-                    >
-                      {item.label}
-                    </a>
-                  ) : (
-                    <ScrollLink
-                      key={item.href}
-                      to={item.href}
-                      smooth={true}
-                      duration={500}
-                      offset={-70}
-                      onClick={closeMenu}
-                      className="text-4xl text-white px-1.5 py-2.5  hover:text-gray-400 hover:cursor-pointer transition-colors  flex items-center gap-3"
-                    >
-                      {item.label}
-                    </ScrollLink>
-                  )
-                )}
-              </div>
+                <div>
+                  <a href="https://www.linkedin.com/in/abdul-majeed-nurudeen-78266a182/">
+                    LinkedIn
+                  </a>
+                </div>
 
-              <div className="flex flex-col gap-10">
-                <h2 className="text-2xl px-1.5 py-4 text-gray-400 border-b border-gray-400">
-                  Socials
-                </h2>
-                <div className="flex flex-wrap gap-6 text-white">
-                  <div>
-                    <a href="https://web.facebook.com/profile.php?id=100080955712476">
-                      Facebook
-                    </a>
-                  </div>
-
-                  <div>
-                    <a href="https://www.linkedin.com/in/abdul-majeed-nurudeen-78266a182/">
-                      LinkedIn
-                    </a>
-                  </div>
-
-                  <div>
-                    <a href="https://web.facebook.com/profile.php?id=100080955712476">
-                      Twitter ( X )
-                    </a>
-                  </div>
+                <div>
+                  <a href="https://web.facebook.com/profile.php?id=100080955712476">
+                    Twitter ( X )
+                  </a>
                 </div>
               </div>
             </div>
-          </motion.div>
-        </div>
-      </AnimatePresence>
+          </div>
+        </motion.div>
+      </div>
     </nav>
   );
 };
