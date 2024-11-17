442fcb56b317d6d1d7b23f1ab2f3b5f035304255
Nurudeen <abdulmajeednurudeen48@gmail.com>
2024-11-17 19:03:42 +0000

    added vercel config and updated the errorpage to show the type of error and also styled it

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,12 +1,26 @@
-import { Link } from 'react-router-dom';
+import { useRouteError, Link } from 'react-router-dom';
+import { ChevronLeft } from 'lucide-react';
 
 const ErrorPage = () => {
+  const error = useRouteError();
+
   return (
-    <div>
-      <h1>Oh no, this route doesn't exist!</h1>
-      <Link to="/">
-        You can go back to the home page by clicking here, though!
-      </Link>
+    <div className="flex flex-col items-center justify-center min-h-screen  bg-gray-200 font-sans">
+      <div className="text-center p-6 mx-4 bg-white shadow-md rounded-lg">
+        <h1 className="text-3xl font-bold text-red-600 mb-4">
+          Oh noo! There is an error
+        </h1>
+        <p className="text-gray-700 mb-4">
+          {error?.statusText || error?.message || 'Something went wrong!'}
+        </p>
+        <Link
+          to="/"
+          className="inline-flex items-center px-4 py-2 bg-black text-white rounded-md border hover:bg-white hover:border hover:border-black hover:text-black transition-colors duration-500 ease-in-out"
+        >
+          <ChevronLeft size={20} className="mr-2" />
+          Go Back to Home
+        </Link>
+      </div>
     </div>
   );
 };
