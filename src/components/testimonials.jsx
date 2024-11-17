42d4332184b565a44b82202cd04b76069f6e1e09
Nurudeen <abdulmajeednurudeen48@gmail.com>
2024-11-17 09:57:42 +0000

    updated testimonials to

--- src/components/testimonials.jsx
+++ src/components/testimonials.jsx
@@ -41,10 +41,13 @@ const TestimonialCarousel = () => {
   };
 
   return (
-    <div className="max-w-4xl mx-auto px-4 py-16 font-urbanist">
-      <div className="relative">
+    <div className="relative max-w-6xl px-6 md:px-12 mx-auto font-urbanist flex flex-col gap-6 mb-16">
+      <div className="max-w-6xl px-6 md:px-12 font-urbanist">
+        <h1 className="text-4xl md:text-5xl font-medium">Testimonials</h1>
+      </div>
+      <div>
         {/* Main Content */}
-        <div className="flex flex-col items-center mx-auto text-center w-5/6 py-4 md:w-3/4 md:shadow-none md:border-none shadow-md border border-gray-200 rounded-xl">
+        <div className="flex flex-col items-center mx-auto text-center w-full p-4 md:w-3/4 md:shadow-none md:border-none shadow-md border border-gray-200 rounded-xl">
           {/* Profile Image */}
           <div className="w-24 h-24 mb-6">
             <img
